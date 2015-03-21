60cb2c3106785fa79e231ec49ba2cfa4946a584f
Genki Marshall <genkimarshall@gmail.com>
2015-03-21 15:41:54 -0400

    Get and display dining hall list from RedAPI

--- app/src/main/java/is/genki/bigredapp/android/MainActivity.java
+++ app/src/main/java/is/genki/bigredapp/android/MainActivity.java
@@ -1,15 +1,31 @@
 package is.genki.bigredapp.android;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,7 +36,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ListViewFragment())
                     .commit();
         }
     }
@@ -49,18 +65,115 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * Fragment containing a ListView of dining halls.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class ListViewFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        private ListView listView;
+
+        public ListViewFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            listView = (ListView) rootView.findViewById(R.id.listView);
+
+            // https://developer.android.com/training/basics/network-ops/connecting.html#connection
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                String diningListUrl = "http://redapi-tious.rhcloud.com/dining";
+                new DisplayDiningHallsTask().execute(diningListUrl);
+            } else {
+                Toast.makeText(getActivity(), "No Internet Connection", Toast.LENGTH_LONG).show();
+            }
+
             return rootView;
+        }
+
+        // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+        // URL string and uses it to create an HttpUrlConnection. Once the connection
+        // has been established, the AsyncTask downloads the contents of the webpage as
+        // an InputStream. Finally, the InputStream is converted into a string, which is
+        // displayed in the UI by the AsyncTask's onPostExecute method.
+        // https://developer.android.com/training/basics/network-ops/connecting.html#AsyncTask
+        private class DisplayDiningHallsTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... urls) {
+
+                // params comes from the execute() call: params[0] is the url.
+                try {
+                    return downloadUrl(urls[0]);
+                } catch (IOException e) {
+                    Toast.makeText(getActivity(), "Unable to retrieve data", Toast.LENGTH_LONG).show();
+                    return null;
+                }
+            }
+
+            // onPostExecute displays the results of the AsyncTask.
+            @Override
+            protected void onPostExecute(String result) {
+                // convert result to JSON, get string array
+                try {
+                    JSONArray jsonArray = new JSONArray(result);
+                    ArrayList<String> diningList = new ArrayList<>();
+                    int len = jsonArray.length();
+                    for (int i=0;i<len;i++){
+                        diningList.add(jsonArray.get(i).toString());
+                    }
+
+                    // http://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                            android.R.layout.simple_list_item_1, diningList);
+                    listView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(), "Data has invalid format", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        // Given a URL, establishes an HttpUrlConnection and retrieves
+        // the web page content as a InputStream, which it returns as
+        // a string.
+        // https://developer.android.com/training/basics/network-ops/connecting.html#download
+        private String downloadUrl(String myUrl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                return readIt(is, len);
+            } finally {
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        // Reads an InputStream and converts it to a String.
+        // https://developer.android.com/training/basics/network-ops/connecting.html#stream
+        public String readIt(InputStream stream, int len) throws IOException {
+            Reader reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
         }
     }
 }
