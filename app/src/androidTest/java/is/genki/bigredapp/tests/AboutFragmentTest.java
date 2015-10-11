0c245db29d825ac94c02232397aa004dc32124b1
Trevor Edwards <treebark12002@yahoo.com>
2015-10-10 23:45:30 -0400

    update test case

--- app/src/androidTest/java/is/genki/bigredapp/tests/AboutFragmentTest.java
+++ app/src/androidTest/java/is/genki/bigredapp/tests/AboutFragmentTest.java
@@ -1,35 +1,49 @@
 package is.genki.bigredapp.tests;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
 
 import is.genki.bigredapp.android.AboutFragment;
+import is.genki.bigredapp.android.MainActivity;
 import is.genki.bigredapp.android.R;
 
 /**
  * A sample of the testing we could be doing on an activity that doesn't need testing.
  * https://developer.android.com/training/activity-testing/activity-basic-testing.html
  */
-public class AboutActivityTest extends ActivityInstrumentationTestCase2<AboutFragment> {
+public class AboutFragmentTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private AboutFragment myAboutActivity;
+    private MainActivity myMainActivity;
     private TextView aboutText;
 
-    public AboutActivityTest(){
-        super(AboutFragment.class);
+    public AboutFragmentTest(){
+        super(MainActivity.class);
     }
 
     public void testPreconditions() {
-        assertNotNull("myAboutActivity is null", myAboutActivity);
+        assertNotNull("myAboutActivity is null", myMainActivity);
         assertNotNull("aboutText is null", aboutText);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        myAboutActivity = getActivity();
+        myMainActivity = getActivity();
+
+        Bundle args = new Bundle();
+        Fragment fragment = new AboutFragment();
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        myMainActivity.getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+        //wait for it to load
+        Thread.sleep( 5000 );
         aboutText =
-                (TextView) myAboutActivity
+                (TextView) myMainActivity
                         .findViewById(R.id.textView2);
     }
 
@@ -38,7 +52,7 @@ public class AboutActivityTest extends ActivityInstrumentationTestCase2<AboutFra
      */
     public void testAboutText() {
         final String expected =
-                myAboutActivity.getString(R.string.about_description);
+                myMainActivity.getString(R.string.about_description);
         final String actual = aboutText.getText().toString();
         assertEquals(expected, actual);
     }
